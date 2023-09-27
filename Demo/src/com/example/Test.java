5735bbc5c994f3f70a6c0400036a49ea426c073a
Ankita <aalandikar@gmail.com>
2023-09-27 21:05:58 +0530

    changes in code for ArrayList in Test.java

--- Demo/src/com/example/Test.java
+++ Demo/src/com/example/Test.java
@@ -7,12 +7,13 @@ import java.util.List;
 public class Test {
 
 	public static void main(String[] args) {
-		List list =new ArrayList<>();
-		list.add(10);
-		list.add(20);
-		list.add(30);
-		list.add(40);
-		list.add(50);
+		List <String>list =new ArrayList<>();
+		list.add("10");
+		list.add("20");
+		list.add("30");
+		list.add("40");
+		list.add("50");
+		System.out.println("List==>" +list);
 
 	}
 
